5a7316a07a4e28ee66c50d60254ae5ed9413019a
Vinay <vinay.patilashok@gmail.com>
2020-02-15 15:13:02 +0000

    Commited the class file for the first time

---
+++ com/vinay/github/Calculator.java
@@ -0,0 +1,40 @@
+package com.vinay.github;
+
+public class Calculator {
+
+	int a,b;
+	int c;
+		
+	int add() {
+		a=20;
+		b=30;
+		c=a+b;
+		return c;		
+	}
+	
+	int sub() {
+		a=50;
+		b=10;
+		c=a-b;
+		return c;
+	}
+	
+	int mul() {
+		a=20;
+		b=40;
+		c=a*b;
+		return c;
+		
+	}
+	
+		public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+			Calculator calc = new Calculator();
+			calc.add();
+			System.out.println(calc.add());
+			System.out.println(calc.sub());
+			System.out.println(calc.mul());
+	}
+
+}
